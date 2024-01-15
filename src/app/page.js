583ce1e11c5a434173029714dc9ba9ed617601b0
Claire Mather <clairemather03@gmail.com>
2024-01-14 20:29:05 -0500

    restarting

--- src/app/page.js
+++
@@ -1,9 +0,0 @@
-import Image from 'next/image'
-
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gray-700">
-      <div className="text-3xl font-sans">Under Construction...</div>
-    </main>
-  )
-}
